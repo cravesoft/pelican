65fd5e1c04b8fec4c9bbaddc46769991b94a448d
Olivier Crave <cravesoft@gmail.com>
2013-02-23 16:55:42 +0100

    Fixed indentiation and missing parenthesis

--- pelican/plugins/paypal_donate_button.py
+++ pelican/plugins/paypal_donate_button.py
@@ -20,14 +20,14 @@ from pelican import signals
 
 
 class PayPalDonateButton():
-import textwrap
+    import textwrap
     FORM_HTML = textwrap.dedent("""\
             <form action="https://www.paypal.com/cgi-bin/webscr" method="post">
                 <input type="hidden" name="cmd" value="_s-xclick">
                 <input type="hidden" name="encrypted" value="{0}">
                 <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donate_LG.gif" border="0" name="submit" alt="PayPal - The safer, easier way to pay online!">
                 <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
-            </form>"""
+            </form>""")
 
     """
         A class created to generate PayPal's html snippet
